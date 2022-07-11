5bf212776e65f5adabe79540d23cda5ec370a797
Dewa Gede Trika Meranggi <50865141+Tri334@users.noreply.github.com>
2022-07-11 20:58:48 +0800

    Update service_api.dart

--- lib/view/home/bloc/api/service_api.dart
+++ lib/view/home/bloc/api/service_api.dart
@@ -7,15 +7,15 @@ part 'popular_api.dart';
 
 class ApiService {
   Future<List<Popular>> getPopular() async {
-    final response = await get(
-        Uri.parse("https://mockend.com/Tri334/fakeApiMocked/popular?limit=10"));
+    final response = await get(Uri.parse(
+        "https://mockend.com/Tri334/explore_bali_island_flutter/popular?limit=10"));
     final data = popularFromJson(response.body);
     return data.toList();
   }
 
   Future<List<Nearby>> getNearby() async {
-    final response = await get(
-        Uri.parse("https://mockend.com/Tri334/fakeApiMocked/nearby?limit=10"));
+    final response = await get(Uri.parse(
+        "https://mockend.com/Tri334/explore_bali_island_flutter/nearby?limit=10"));
     final nearby = nearbyFromJson(response.body);
 
     return nearby;
